7a1d909574a819a01e746e7dc8ad7ded374a1362
JoeYo24 <jjsteffen@hotmail.com>
2024-02-06 16:20:02 -0500

    subheader section finished

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -8,10 +8,41 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-
-      <h1 className="text-center pt-1 w-100">Deals</h1>
+      <h1 className="text-center pt-1 w-100 dealsHeader">Deals</h1>
       <div className="d-flex">
         <DealCarousel />
+      </div>
+      <div className="d-flex container mb-5">
+        <div className="row w-100">
+          <div className="col-12 col-lg-3">
+            <a href="#" className="card-link">
+              <div className="card-container rounded">
+                Unlimited Club        
+              </div>
+            </a>
+          </div>
+          <div className="col-12 col-lg-3">
+            <a href="#" className="card-link">
+              <div className="card-container rounded">
+                Gift Cards
+              </div>
+            </a>  
+          </div>
+          <div className="col-12 col-lg-3">
+            <a className="card-link" href="#">
+              <div className="card-container rounded">
+                Book a Detail
+              </div>
+            </a>  
+          </div>
+          <div className="col-12 col-lg-3">
+            <a className="card-link" href="#">
+              <div className="card-container rounded">
+                Retail Products
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
